d3806ca5defaf357db5192e15afa2ed7bb48d09a
AakritiBagchi <aak28.ab@gmail.com>
2020-02-12 23:23:45 +0000

    finished html setup and started css

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/// when should I add <button class="listen">Listen</button> in html?
+
+    let title = $(".title").val();
+    let length = $(".length").val();
+    let url = $(".url").val();
+    let artist = $(".artist").val();
+    let songLink = $(".songLink").val();
+    
+$(".add").click(function() {
+    
+
+    
+    
+});
