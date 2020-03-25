b2de48fc7200e317466bd9d34f2410ede5db37f2
AakritiBagchi <aak28.ab@gmail.com>
2020-03-25 23:03:14 +0000

    finished project

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
+
+
 /// when should I add <button class="listen">Listen</button> in html?
 
+    let songs = [];
+      
+$(".add").click(function() {
     let title = $(".title").val();
     let length = $(".length").val();
     let url = $(".url").val();
     let artist = $(".artist").val();
     let songLink = $(".songLink").val();
     
-$(".add").click(function() {
+    let song= {
+        title: title,
+        length: length,
+        url: url,
+        artist: artist,
+        songLink: songLink
+    };
     
-
+    songs.push(song);
+    function addToDiv(div, property){
+        $(div).append("<p>" + song[property] + "</p>");
+    }
+    
+    addToDiv(".songTitle","title");
+    addToDiv(".songLength","length");
+    addToDiv(".songURL","url");
+    addToDiv(".artistName","artist");
+    addToDiv(".songLink","songLink");
+    
+    function listen(link){
+        $(".listen").append("<button onclick=link>" + "Click Here" +"</button>");
+    }
+    
+    });
     
     
-});
